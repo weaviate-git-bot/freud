1ca23a20b6130fa36d0fd058780484ae7270232b
Fredrik Randsborg Bølstad <69513590+fredrikrab@users.noreply.github.com>
2023-06-28 08:48:50 +0200

    replace duplicate initialization of tRPC
    See https://trpc.io/docs/server/routers#initialize-trpc

--- src/server/api/routers/openai.ts
+++ src/server/api/routers/openai.ts
@@ -1,7 +1,6 @@
 import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { Configuration, OpenAIApi } from "openai";
-import { initTRPC } from "@trpc/server";
+import { createTRPCRouter, publicProcedure } from "../trpc";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -9,21 +8,19 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-export const t = initTRPC.create();
-export const openAIRouter = t.router({
-  chat: t.procedure
+export const openAIRouter = createTRPCRouter({
+  chat: publicProcedure
     // using zod schema to validate and infer input values
     .input(
       z.object({
         query: z.string(),
-      }),
+      })
     )
     .mutation(async ({ input }) => {
       const chatCompletion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: input.query }],
       });
-      console.log(chatCompletion.data.choices[0].message.content)
       return {
         message: chatCompletion.data.choices[0].message.content,
       };
