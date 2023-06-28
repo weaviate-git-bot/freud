c42176511705d55d3e7fdf43e929819df2ff115e
Fredrik Randsborg Bølstad <69513590+fredrikrab@users.noreply.github.com>
2023-06-28 10:14:37 +0200

    add descriptions to Role enum + use Message type in OpenAIRouter

--- src/interfaces/chat.ts
+++ src/interfaces/chat.ts
@@ -1,9 +1,9 @@
 // Defines an enum with the same values as the OpenAI API
 // https://platform.openai.com/docs/guides/gpt/chat-completions-api
 export enum Role {
-  System,
-  User,
-  Assistant,
+  System = "system",
+  User = "user",
+  Assistant = "assistant",
 }
 
 export interface Message {
--- src/server/api/routers/openai.ts
+++ src/server/api/routers/openai.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { Configuration, OpenAIApi } from "openai";
 import { createTRPCRouter, publicProcedure } from "../trpc";
+import { Role, type Message } from "~/interfaces/chat";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -21,8 +22,12 @@ export const openAIRouter = createTRPCRouter({
         model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: input.query }],
       });
+      const message: Message = {
+        role: Role.Assistant,
+        content: chatCompletion.data.choices[0].message.content!, //TODO linter complains about possibly undefined value
+      };
       return {
-        message: chatCompletion.data.choices[0].message.content,
+        message,
       };
     }),
 });
