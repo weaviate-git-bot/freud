ababff20e0dc9701fb1a5a95eb7652440a01765f
jodderns lokale pc brukernavn :) <kristtjo@stud.ntnu.no>
2023-07-17 16:17:18 +0200

    Pulled out into MessageComponent

---
+++ src/components/MessageComponent.tsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import Image from 'next/image';
+import { type Message, Role } from '~/interfaces/message';
+import { colors } from '~/stitches/colors';
+import SourceComponent from './sourceComponent';
+
+type Prop = {
+  message: Message,
+  children: React.ReactNode,
+}
+
+const AVATAR_IMAGE_SIZE = 50;
+
+const MessageComponent = ({ message, children }: Prop) => {
+  return (
+    <div
+      className="container border-b-2 border-gray900 py-10"
+    >
+      {message.role === Role.User ? (
+        <div className="flex items-start space-x-4">
+          <Image
+            className="mt-3"
+            src="/chatter_avatar_2.png"
+            alt="This is text"
+            width={AVATAR_IMAGE_SIZE}
+            height={AVATAR_IMAGE_SIZE}
+          />
+          <p className="pt-5">
+            {message.content}
+          </p>
+        </div>
+      ) : (
+        <div>
+          <div className="relative">
+            <Image
+              className="float-left mr-4"
+              src="/sigmund_freud_avatar.png"
+              alt="This is text"
+              width={AVATAR_IMAGE_SIZE}
+              height={AVATAR_IMAGE_SIZE}
+            />
+            {children}
+            <p
+              color={colors.beige400}
+            >
+              {message.content}
+            </p>
+          </div>
+
+          <div className="mb-3">
+            {message.sources == undefined ||
+            message.sources?.length == 0 ? (
+              <p className="bold py-2 font-bold text-yellow550">
+                Fant ingen kilder til dette spørsmålet
+              </p>
+            ) : (
+              <div>
+                <p className="bold py-2 font-bold">Kilder</p>
+
+                <ul>
+                  {message.sources.map((source, sourceIdx) => {
+                    return (
+                      <SourceComponent
+                        key={sourceIdx}
+                        source={source}
+                      />
+                    );
+                  })}
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default MessageComponent
--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,86 +1,20 @@
 import React from "react";
 import FeedbackComponent from "./feedbackComponent";
-import { colors } from "~/stitches/colors";
-import { Role, type Message } from "~/interfaces/message";
-import Image from "next/image";
-import SourceComponent from "./sourceComponent";
+import { type Message } from "~/interfaces/message";
+import MessageComponent from "./MessageComponent";
 
 type Prop = {
   messages: Message[];
 };
 
-const AVATAR_IMAGE_SIZE = 50;
-
 const MessageList = ({ messages }: Prop) => {
   return (
     <div>
-      {messages.map((message, idx) => {
-        return (
-          <div
-            key={idx.toString()}
-            className="container border-b-2 border-gray900 py-10"
-          >
-            {message.role === Role.User ? (
-              <div key={idx} className="flex items-start space-x-4">
-                <Image
-                  className="mt-3"
-                  src="/chatter_avatar_2.png"
-                  alt="This is text"
-                  width={AVATAR_IMAGE_SIZE}
-                  height={AVATAR_IMAGE_SIZE}
-                />
-                <p className="pt-5" key={idx}>
-                  {message.content}
-                </p>
-              </div>
-            ) : (
-              <div key={idx}>
-                <div className="relative">
-                  <Image
-                    className="float-left mr-4"
-                    src="/sigmund_freud_avatar.png"
-                    alt="This is text"
-                    width={AVATAR_IMAGE_SIZE}
-                    height={AVATAR_IMAGE_SIZE}
-                  />
-                  <FeedbackComponent chat={messages} />
-                  <p
-                    color={colors.beige400}
-                    className=""
-                    key={"reply-" + idx.toString()}
-                  >
-                    {message.content}
-                  </p>
-                </div>
-
-                <div className="mb-3">
-                  {message.sources == undefined ||
-                  message.sources?.length == 0 ? (
-                    <p className="bold py-2 font-bold text-yellow550">
-                      Fant ingen kilder til dette spørsmålet
-                    </p>
-                  ) : (
-                    <div>
-                      <p className="bold py-2 font-bold">Kilder</p>
-
-                      <ul>
-                        {message.sources.map((source, sourceIdx) => {
-                          return (
-                            <SourceComponent
-                              key={sourceIdx}
-                              source={source}
-                            />
-                          );
-                        })}
-                      </ul>
-                    </div>
-                  )}
-                </div>
-              </div>
-            )}
-          </div>
-        );
-      })}
+      {messages.map( (message, idx) => (
+        <MessageComponent  message={message} key={idx}>
+          <FeedbackComponent chat={messages}/>
+        </MessageComponent>
+      ))}
     </div>
   );
 };
