97a3fec896f27ba2317f7bd274b0a152c2d3a012
David Lekve <david.lekve@gmail.com>
2023-07-20 13:02:06 +0200

    parse refs to anchor tags

--- src/components/MessageComponent.tsx
+++ src/components/MessageComponent.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import { type Message, Role } from '~/interfaces/message';
 import { colors } from '~/stitches/colors';
 import SourceComponent from './SourceItem';
+import Link from 'next/link';
 
 type Prop = {
   message: Message,
@@ -13,6 +14,52 @@ const AVATAR_IMAGE_SIZE = 50;
 
 const MessageComponent = ({ message, children }: Prop) => {
   console.log(message.content)
+
+
+
+  const formatLinks = (input: string): React.JSX.Element => {
+    try {
+
+      var regex = /\[[0-9]+\]/g;
+
+      const goodspaces = input.replaceAll("\n", " \n")
+
+      const splittext = goodspaces.split(' ');
+
+      let outputlist: any[] = []
+
+      let mystring = "";
+      splittext.map((split) => {
+        if (regex.test(split)) {
+          outputlist.push(mystring)
+          mystring = ""
+          for (let i = 1; i <= message.sources!.length; i++) {
+            if (parseInt(split.charAt(1)) == i) {
+
+              outputlist.push(<a href="#" onClick={() => { }}>[{i}].</a>)
+            }
+          }
+        } else {
+          mystring += split + " ";
+
+        }
+      })
+
+      const output = <p className='whitespace-pre-wrap'>
+        {outputlist}
+      </p>
+
+
+      return output;
+    }
+    catch {
+      //Code above is bad. So if it breaks, sources wont be clickable.
+      console.log("")
+      return <p>input</p>
+    }
+  }
+
+
   return (
     <div
       className="container border-b-2 border-gray900 py-10"
@@ -45,7 +92,7 @@ const MessageComponent = ({ message, children }: Prop) => {
               color={colors.beige400}
               className="whitespace-pre-wrap"
             >
-              {message.content}
+              {formatLinks(message.content)}
             </p>
           </div>
 
